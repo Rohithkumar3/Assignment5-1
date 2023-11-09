02ca09eff5092955bf5a5a5262bf51f975660396
Rohithkumar <rohithkumargande@gmail.com>
2023-11-09 15:05:46 +0530

    part1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Rohithkumar";
+            int age = 23;
+            double height = 180.5;
+            Console.WriteLine("My Name is " + name);
+            Console.WriteLine("age "+age);
+            Console.WriteLine("hight "+height);
+            Console.ReadKey();
+        }
+    }
+}
